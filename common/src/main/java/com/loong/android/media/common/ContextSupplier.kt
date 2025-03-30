41500888d95b9a8fe35d7b5c04201a4b7ede13c4
loong <1536463948@qq.com>
2025-03-30 10:09:39 +0800

    feat: 补充注释

--- common/src/main/java/com/loong/android/media/common/ContextSupplier.kt
+++ common/src/main/java/com/loong/android/media/common/ContextSupplier.kt
@@ -3,12 +3,24 @@ package com.loong.android.media.common
 import android.app.Application
 import android.content.Context
 
+/**
+ * 提供全局Application
+ */
 object ContextSupplier {
+    /**
+     * application实例
+     */
     private lateinit var application: Application
 
+    /**
+     * 初始化
+     */
     internal fun init(context: Context) {
         application = context as Application
     }
 
+    /**
+     * 获取application实例
+     */
     fun get() = application
 }
