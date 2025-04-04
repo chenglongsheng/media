6dc49155a18eb5258191bdd9458d0cb38893ec5f
loong <1536463948@qq.com>
2025-04-04 09:20:13 +0800

    feat: 初始化模块

--- app/src/main/java/com/loong/android/media/App.kt
+++ app/src/main/java/com/loong/android/media/App.kt
@@ -1,14 +1,14 @@
 package com.loong.android.media
 
 import android.app.Application
-import com.loong.android.media.common.TLog
+import com.loong.android.media.common.MediaManager
 import com.loong.android.media.player.PlayManager
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        TLog.i("onCreate: ")
+        MediaManager.instance.init(this)
         PlayManager.init(this)
     }
 
